f132a62b1cb37ff31872104b39d2bacab1cb0b59
Maxime2 SIMON <maxisimo@e1r7p19.42.fr>
2018-10-23 16:53:38 +0200

    change main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: maxisimo <maxisimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/16 11:54:23 by thbernar          #+#    #+#             */
-/*   Updated: 2018/10/23 16:39:26 by maxisimo         ###   ########.fr       */
+/*   Updated: 2018/10/23 16:53:13 by maxisimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,18 @@ int			main(int ac, char **av)
 {
 	t_app	app;
 
-	(void)ac;
-	app.fname = av[1];
-	ft_app_init(&app);
-	app.mlx = mlx_init();
-	app.win = mlx_new_window(app.mlx, WIN_W, WIN_H, "Wolf3D");
-	raycasting(&app);
-	mlx_hook(app.win, 2, (1L << 0), ft_key_press, &app);
-	mlx_hook(app.win, 3, (1L << 1), ft_key_release, &app);
-	mlx_hook(app.win, 17, (1L << 17), ft_close, &app);
-	mlx_loop_hook(app.mlx, ft_move, &app);
-	mlx_loop(app.mlx);
+	if (ac == 2)
+	{
+		app.fname = av[1];
+		ft_app_init(&app);
+		app.mlx = mlx_init();
+		app.win = mlx_new_window(app.mlx, WIN_W, WIN_H, "Wolf3D");
+		raycasting(&app);
+		mlx_hook(app.win, 2, (1L << 0), ft_key_press, &app);
+		mlx_hook(app.win, 3, (1L << 1), ft_key_release, &app);
+		mlx_hook(app.win, 17, (1L << 17), ft_close, &app);
+		mlx_loop_hook(app.mlx, ft_move, &app);
+		mlx_loop(app.mlx);
+	}
 	return (0);
 }
