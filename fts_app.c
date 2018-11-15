1761954ad61fdddb0ec2d6fee9c9e4b727ffa43d
Maxime2 SIMON <maxisimo@e1r3p3.42.fr>
2018-11-15 16:10:00 +0100

    6tex

--- fts_app.c
+++ fts_app.c
@@ -6,7 +6,7 @@
 /*   By: maxisimo <maxisimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/11 18:43:12 by thbernar          #+#    #+#             */
-/*   Updated: 2018/11/15 15:25:14 by maxisimo         ###   ########.fr       */
+/*   Updated: 2018/11/15 16:05:53 by maxisimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -144,6 +144,6 @@ void		ft_app_init(t_app *app)
 	load_bmp(&app->textures[3], "textures/red.bmp");
 	load_bmp(&app->textures[4], "textures/metal1.bmp");
 	load_bmp(&app->textures[5], "textures/metal2.bmp");
-	load_bmp(&app->textures[6], "textures/wall7.bmp");
-	load_bmp(&app->textures[7], "textures/wall8.bmp");
+	load_bmp(&app->textures[6], "textures/bois1.bmp");
+	load_bmp(&app->textures[7], "textures/bois2.bmp");
 }
