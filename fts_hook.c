bdd1cd9d3ca7e239e851449bc9ccfecb6f292cf6
Bernard <thomas.bernard@outlook.fr>
2018-11-15 18:10:56 +0100

    startscreen end

--- fts_hook.c
+++ fts_hook.c
@@ -19,30 +19,33 @@ static void	ft_start_screen(t_app *a)
 	t_color	c1;
 	t_coord p;
 	int		n[3];
+	double	sin_factor;
 
 	p.x = 0;
 	a->img = mlx_new_image(a->win, WIN_W, WIN_H);
 	a->img_data = mlx_get_data_addr(a->img, &n[0], &n[1], &n[2]);
 	a->loop = a->loop + 0.02;
-	c1.r = fabs(sin(a->loop)) * 255;
-	c1.g = fabs(sin(a->loop)) * 255;
-	c1.b = fabs(sin(a->loop)) * 255;
+	sin_factor = fabs(sin(a->loop));
+	c1.r = sin_factor * 255;
+	c1.g = sin_factor * 255;
+	c1.b = sin_factor * 255;
 	p.y = 1;
-	while (p.y < a->startscreen_logo.height)
+	while (p.y < a->startscreen_logo.height * 3)
 	{
 		p.x = 1;
-		while (p.x < a->startscreen_logo.width)
+		while (p.x < a->startscreen_logo.width * 3)
 		{
-			c = get_pixel_color(&a->startscreen_logo, p.x, p.y);
+			c = get_pixel_color(&a->startscreen_logo, p.x / 3, p.y / 3);
 			clr = ft_rgb_to_hex(c);
-			ft_memcpy(a->img_data + 4 * WIN_W * p.y + p.x * 4, &clr, sizeof(int));
+			ft_memcpy(a->img_data + 4 * WIN_W * (p.y + WIN_H / 2 - (a->startscreen_logo.height * 3)) + (p.x * 4 + (WIN_W + a->startscreen_logo.width * 3)), &clr, sizeof(int));
 			p.x++;
 		}
 		p.y++;
 	}
 	mlx_put_image_to_window(a->mlx, a->win, a->img, 0, 0);
-	mlx_string_put(a->mlx, a->win, 450, 300, ft_rgb_to_hex(c1), START);
+	mlx_string_put(a->mlx, a->win, WIN_W / 2 - 150, WIN_H / 2 + 100, ft_rgb_to_hex(c1), START);
 	mlx_destroy_image(a->mlx, a->img);
+	mlx_do_sync(a->mlx);
 }
 
 int			expose_hook(t_app *a)
